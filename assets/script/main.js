317418b5420735cb9564616c75a3ea7a5ba81492
ina <r.ina.kimiduka@gmail.com>
2025-07-29 08:39:55 +0900

    修正・セクション追加

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $(document).ready(function () {
+	// メニューボタン
 	$('.header__menu-btn a').on('click', function (e) {
 		e.preventDefault();
 		$('#js-drawer').slideToggle();
@@ -10,4 +11,13 @@ $(document).ready(function () {
 			if($('#js-drawer').is(':visible')) $('#js-drawer').slideToggle();
 		}
 	});
+
+	// タブ切り替え
+    const $tabs = $('.tab');
+    $tabs.on('click', function() {
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+        const index = tabs.index(this);
+        $('.content').removeClass('show').eq(index).addClass('show');
+    });
 });
