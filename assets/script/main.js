6e4f3b5423133535d19a60891dcd0253823c01c3
ina <r.ina.kimiduka@gmail.com>
2025-08-11 13:27:04 +0900

    修正稿提出

--- assets/script/main.js
+++ assets/script/main.js
@@ -18,9 +18,9 @@ $(document).ready(function () {
 	// タブ切り替え
 	const $tabs = $('.target__tab');
 	$tabs.on('click', function() {
-			$('.active').removeClass('active');
-			$(this).addClass('active');
-			const index = $tabs.index(this);
-			$('.target__content').removeClass('show').eq(index).addClass('show');
+		$('.active').removeClass('active');
+		$(this).addClass('active');
+		const index = $tabs.index(this);
+		$('.target__content').removeClass('show').eq(index).addClass('show');
 	});
 });
